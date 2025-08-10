550d4580e664f1a090b7f33a0220ae4eb42a0d0a
Radin <110737910+radG1t@users.noreply.github.com>
2025-08-10 17:58:10 +0330

    produc page ..

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:narsat/main.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -7,16 +8,58 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('محصولات'),
         backgroundColor: Colors.red[700],
         foregroundColor: Colors.white,
-        automaticallyImplyLeading: false, // برای حذف دکمه بازگشت
+        automaticallyImplyLeading: false,
       ),
-      body: const Center(
-        child: Text(
-          'شما با موفقیت وارد شدید!',
-          style: TextStyle(fontSize: 22.0, color: Colors.black54),
+
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.red[700]),
+              child: const Text(
+                'منو',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('خانه'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('تنظیمات'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
+      ),
+      body: GridView.builder(
+        physics: const BouncingScrollPhysics(
+          parent: AlwaysScrollableScrollPhysics(),
+        ),
+
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+        ),
+        itemBuilder: (context, index) {
+          return ProductCard(
+            image: "",
+            title: "title goes here",
+            price: "1230123",
+          );
+        },
+        itemCount: 15,
       ),
     );
   }
